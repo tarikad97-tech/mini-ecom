ca875dc4e120ba25b43b68ad027f4726ecddbd15
saaddhm <saaddhm3@gmail.com>
2026-02-03 15:59:58 +0100

    modification dossier admin partie frondoffice

---
+++ Front-office/admin/sidebar.php
@@ -0,0 +1,7 @@
+<div class="sidebar">
+        <a href="dashboard.php">Dashboard</a>
+        <a href="categories.php">Categories</a>
+        <a href="products.php">Products</a>
+        <a href="orders.php">Orders</a>
+        <a href="logout.php">Logout</a>
+    </div>
