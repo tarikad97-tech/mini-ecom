34d05cc6e93e2a9f3cea3a0c5a595829e8a82b18
AD2004 <elkoubaiadnane2004@gmail.com>
2026-02-04 09:35:33 +0100

    add orders jason

--- Back-office/admin/orders.php
+++ Back-office/admin/orders.php
@@ -1,2 +1,14 @@
 <?php
+header("Content-Type: application/json");
+include '../../database/config.php';
+
+
+$sql = "SELECT * FROM orders ";
+$res = mysqli_query($db, $sql);
+ $ordres = [];
+while ($row = mysqli_fetch_assoc($res)) {
+    $ordres[] = $row;
+}
+
+echo json_encode($ordres);
 ?>
